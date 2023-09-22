5c5f3295243ad6427cf7dfcd870774acd474fb93
geraldHamiltonWicks <gerald.hamilton@ufba.br>
2023-09-21 21:42:04 -0300

    adicionando embeddings, e fazendo o back-end funcionar

--- backend/endpoints/answer.py
+++ backend/endpoints/answer.py
@@ -3,49 +3,49 @@ from endpoints.automacao.automacao import load_automacao
 from endpoints.quimica.quimica import load_quimica
 from endpoints.geral.geral import load_geral
 from helpers.get_env_variables import get_openai_api_key, get_hugging_face_token
-from helpers.chunks import get_text_chunks, get_vectors_store
+from helpers.chunks import get_text_chunks, get_vectors_store, get_conversation_chain
 from dotenv import load_dotenv
+import pickle
+import json
 
 def get_answer():
-    openai_api_key = get_openai_api_key()
-    hugging_face_token = get_hugging_face_token()
-
     # Load variables from .env file into environment
-    # load_dotenv()
+    load_dotenv()
 
     # Get the data from the body
     question = request.json['question']
     course = request.json['course']
 
     if course.lower() == "engenharia de controle e automação":
-        # Loading the pdfs from engenharia de controle e automação
-        load_automacao()
+        # Load the vectorstore from a file, containing info from geral and engenharia de automacao
+        with open("./automacao.pkl", "rb") as f:
+            vectorstore = pickle.load(f)
     
     elif course.lower() == "engenharia química":
-        # Loading the pdfs from engenharia química
-        load_quimica()
+        # Load the vectorstore from a file, containing info from geral and engenharia química
+        with open("./quimica.pkl", "rb") as f:
+            vectorstore = pickle.load(f)
     
     else:
         return Response('Incorrect course type', status=400)
 
     # Loading the pdfs from geral
-    text_geral = load_geral()
+    # text_geral = load_geral()
+    # print(text_geral)
 
     # Get the text chunks
-    text_geral_chunks = get_text_chunks(text_geral)
-
-    return text_geral_chunks
+    # text_geral_chunks = get_text_chunks(text_geral)
+    # print(text_geral_chunks)
 
     # Create vector store
-    vectorstore = get_vectors_store(text_geral_chunks)
+    # vectorstore = get_vectors_store(text_geral_chunks)
 
-    # Open a file named 'hello.txt' in write mode ('w')
-    with open('./endpoints/test.txt', 'w', encoding='utf-8') as file:
-    # Write the text 'hello world' to the file
-        file.write(vectorstore)
+    # Create conversation chain
+    conversation_chain = get_conversation_chain(vectorstore)
 
-    return 'Finished'
+    response = conversation_chain({ 'question': question })
+    print(response)
 
-    # Extract the answer, given some question
-    # answer = question
-    # return jsonify({ 'answer': answer })
+    # Get the 'answer' property from the resonde
+    answer = response['answer']
+    return answer
--- backend/helpers/chunks.py
+++ backend/helpers/chunks.py
@@ -1,11 +1,15 @@
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.llms import HuggingFaceHub
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 
 def get_text_chunks(raw_text):
     text_splitter = CharacterTextSplitter(
         separator="\n", # The separator gonnar be single line break
-        chunk_size=1000, # The num of characters of each chunk
+        chunk_size=512, # The num of characters of each chunk
         chunk_overlap=200, # Prevents breaks, when the chunk end at the start of the other
         length_function=len
     )
@@ -13,7 +17,20 @@ def get_text_chunks(raw_text):
     return chunks
 
 def get_vectors_store(text_chunks):
-    # embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    return vector_store
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
